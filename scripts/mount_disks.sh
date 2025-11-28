fa52dd64b87c55793847da1a27a246adda5762b7
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2025-11-28 12:56:14 -0500

    change mount disk to uuid

--- scripts/mount_disks.sh
+++ scripts/mount_disks.sh
@@ -1,5 +1,5 @@
-sudo cryptsetup open /dev/sdb1 extension --type luks
+sudo cryptsetup open 98f868d6-3a25-476c-b34f-ced4c5b6f9b2 extension --type luks
 sudo mount /dev/mapper/extension /mnt/extension
 
-sudo cryptsetup open /dev/sdc backup --type luks
+sudo cryptsetup open 121874b8-4873-446d-bbc9-5e83eb69a35f backup --type luks
 sudo mount /dev/mapper/backup /mnt/backup
