b1930b4d54aa9ad092359de6ef3467f47583a9fa
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2026-01-24 11:58:19 -0500

    Squashed 'apps/budget/src/' content from commit 2ed32c4bc
    git-subtree-dir: apps/budget/src
    git-subtree-split: 2ed32c4bc42a27cd63870f08495ae3d4bd3d7eae

---
+++ packages/api/utils.ts
@@ -0,0 +1,5 @@
+// @ts-ignore: bundle not available until we build it
+import * as bundle from './app/bundle.api.js';
+
+export const amountToInteger = bundle.lib.amountToInteger;
+export const integerToAmount = bundle.lib.integerToAmount;
